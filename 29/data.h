7e1dd3ba594877c2433e8a6df42e64e8cab6cbf9
SIMHANGSUB <simhs93@naver.com>
2018-06-19 22:17:59 +0900

    6.19

---
+++ 29/data.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define sectorsize 512
+
+#define videomaxline 25
+#define videomaxcol 80
+
+#define BACKSPACE 0x08
+#define ENTER 0x13
+
+#define TRUE 1
+#define FALSE 0
+
+#define DEBUG 24
+#define BASEPATHLEN 8
+
+unsigned char keyboard[videomaxcol];
+unsigned short kindex;
+
+unsigned char diskbuffer[sectorsize];
+unsigned char HDDflag;
+
+unsigned short curline; // shell ���� ���� ����
+unsigned short curcol; // shell ���� ���� column
+
+unsigned int timer;
+
+unsigned char path[32];
+unsigned char pathIndex;
+unsigned char dirLevel;
